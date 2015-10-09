23ca2e4fcbe3760b15e065ff4ba470f05df6dca0
Akash Agrawal <akash.agrawal1992@gmail.com>
2015-10-09 16:15:18 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -123,7 +123,7 @@ setup(
             'test/pki/ca/*.0'
             ],
     },
-    install_requires=['OpenSSL'],
+    install_requires=['pyopenssl'],
     extras_require={'subjectAltName_support': 'pyasn1'},
     classifiers=[
         'Development Status :: 3 - Alpha',
